91f2bca6db388c87b9abba485e873918f0ced833
judahdaboss <46258035+judahdaboss@users.noreply.github.com>
2019-10-02 20:49:23 -0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,12 @@
+import discord
+from discord.ext import commands
 
+@bot.event
+async def on_ready():
+  await bot.change_presence(type=discord.ActivityType.watching, activity=discord.Game("Here"))
+  
+@bot.command()
+async def ping(ctx):
+  await ctx.send("Pong!")
+
+bot.run(os.environ['BOT_TOKEN'])
